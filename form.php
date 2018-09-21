54e8198d5fee880b330242c9542f4a36ca066789
linux <manueellg97@gmail.com>
2018-09-21 16:40:28 +0200

    First commit

--- form.php
+++ form.php
@@ -1,11 +1,13 @@
 
-<<html>
+<html>
     <head>
         <title>FORM</title>
     </head>
     <body>
-        <form action="<? echo $_SERVER[PHP_SELF]; ?>">
-            
+        <form action="<? echo $_SERVER[PHP_SELF]; ?>" method="POST">
+            Name:<input type="text" name="name">   
+            Age:<input type="text" name="age" >
+            <input type="submit" value="send">
             
         </form>
     </body>
@@ -13,3 +15,8 @@
 
 <?php
 
+ if(isset($_POST['name']) && !empty($_POST['name'])) {
+      echo "Hola " .$_POST['name']; 
+ } else {
+      echo "Introduce el nombre";
+ }
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //activamos visualitzacion de errores
 ini_set('display_errors','On');
 //incluimos un fichero      
-//include 'cabecera.php';
+include 'form.php';
 
 
 ?>
